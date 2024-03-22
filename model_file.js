b102da96072d2e45088f0a02118128cbb3be6e1b
parkgeonwo <parkkeonwo@naver.com>
2024-03-22 16:30:31 +0900

    first commit

---
+++ model_file.js
@@ -0,0 +1,189 @@
+
+const motorcycles_125cc = [
+  { name: "Z 125", brand:"가와사키", image: "./img/125cc/125-가와사키-z125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "힙스터 125", brand:"몬디알", image: "./img/125cc/125-몬디알-힙스터125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "스프린트 125", brand:"베스파", image: "./img/125cc/125-베스파-스프린트125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "GTS 125", brand:"베스파", image: "./img/125cc/125-베스파-gts125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "크롬웰 125", brand:"브릭스톤", image: "./img/125cc/125-브릭스톤-크롬웰125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "펠스버그 125", brand:"브릭스톤", image: "./img/125cc/125-브릭스톤-펠스버그125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "GSX R 125", brand:"스즈키", image: "./img/125cc/125-스즈키-gsxr125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "GSX S 125", brand:"스즈키", image: "./img/125cc/125-스즈키-gsxs125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "NMAX 125", brand:"야마하", image: "./img/125cc/125-야마하-nmax125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "TRICITY 125", brand:"야마하", image: "./img/125cc/125-야마하-tricity125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "G1", brand:"존테스", image: "./img/125cc/125-존테스-g1.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "스바르트필렌 125", brand:"허스크바나", image: "./img/125cc/125-허스크바나-스필125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "몽키 125", brand:"혼다", image: "./img/125cc/125-혼다-몽키125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "슈퍼커브", brand:"혼다", image: "./img/125cc/125-혼다-슈퍼커브.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "헌터커브", brand:"혼다", image: "./img/125cc/125-혼다-헌터커브.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "CB 125 R", brand:"혼다", image: "./img/125cc/125-혼다-cb125r.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "CBR 125", brand:"혼다", image: "./img/125cc/125-혼다-cbr125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "MSX 125", brand:"혼다", image: "./img/125cc/125-혼다-msx125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "PCX 125", brand:"혼다", image: "./img/125cc/125-혼다-pcx125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "캐드웰 125", brand:"AJS", image: "./img/125cc/125-ajs-캐드웰125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "아퀼라 125", brand:"KR 모터스", image: "./img/125cc/125-kr모터스-아퀼라125.png", url: "https://www.reitwagen.co.kr/d" },
+  { name: "DUKE 125", brand:"KTM", image: "./img/125cc/125-ktm-duke125.png", url: "https://www.reitwagen.co.kr/d" },
+  // 모터사이클 모델 정보를 추가
+
+];
+const motorcycles_quarter = [
+  { name: "모델1", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델2", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델3", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델4", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델5", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델6", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델7", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델8", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델9", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델10", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델11", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델12", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델13", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델14", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델15", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델16", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델17", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델18", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델19", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델20", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  // 모터사이클 모델 정보를 추가
+];
+const motorcycles_middle = [
+  { name: "모델1", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델2", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델3", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델4", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델5", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델6", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델7", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델8", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델9", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델10", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델11", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델12", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델13", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델14", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델15", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델16", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델17", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델18", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델19", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  { name: "모델20", brand:"브랜드", image: "", url: "https://www.reitwagen.co.kr/d" },
+  // 모터사이클 모델 정보를 추가
+];
+
+
+let currentRound = [];
+
+function startRound(startMotorcycles) {
+  if (startMotorcycles.length === 1) {
+    showFinalResult(startMotorcycles[0]);
+    return;
+  }
+
+  const container = document.getElementById('motorcycleContainer');
+  container.innerHTML = '';
+  container.style.display = 'flex';
+
+  const selection = startMotorcycles.splice(0, 2);
+  currentRound = selection;
+
+  selection.forEach(motorcycle => {
+    const div = document.createElement('div');
+    div.classList.add('motorcycle');
+    div.innerHTML = `<img src="${motorcycle.image}" alt="${motorcycle.name}"><div>${motorcycle.brand}</div><div>${motorcycle.name}</div>`;
+    div.onclick = () => selectMotorcycle(motorcycle, startMotorcycles);
+    container.appendChild(div);
+  });
+}
+
+function selectMotorcycle(selected, startMotorcycles) {
+  currentRound.forEach(motorcycle => {
+    if (motorcycle.name === selected.name) {
+      startMotorcycles.push(selected); // 선택된 모터사이클을 다음 라운드에 추가
+    }
+  });
+
+  startRound(startMotorcycles); // 다음 라운드 시작
+}
+
+// 한 개 남았을 때 마지막 결과 보여주기
+function showFinalResult(result) {
+  const resultContainer = document.getElementById('result');
+  const finalMotorcycle = result;
+  console.log(finalMotorcycle);
+  resultContainer.innerHTML = `
+    <div>당신이 고른 1등은</div>
+    <img src="${finalMotorcycle.image}" alt="${finalMotorcycle.name}">
+    <div>${finalMotorcycle.brand}</div>
+    <div>${finalMotorcycle.name}</div>
+    <button onclick="window.open('${finalMotorcycle.url}')" >내가 고른 바이크의 가격은 얼마일까?</button>
+    <button onclick="location.href='./index.html'" >다른 배기량 월드컵도 해보자!</button>
+    <button id="copyUrlBtn">친구한테 공유하기!</button>`;
+  resultContainer.style.display = 'flex';
+  resultContainer.style.flexDirection = 'column';
+  // onclick="location.href='./index.html'"
+
+  const motorcycleContainer = document.getElementById('motorcycleContainer');
+  motorcycleContainer.style.display = 'none';
+  const choiceInfoText = document.getElementById('choice-info-text');
+  choiceInfoText.style.display = 'none';
+
+  // URL 복사 버튼 이벤트 리스너 추가
+  document.getElementById('copyUrlBtn').addEventListener('click', copyCurrentUrl);  
+}
+
+// url 복사하기
+function copyCurrentUrl() {
+  navigator.clipboard.writeText(window.location.href)
+    .then(() => {
+      alert('링크가 복사되었습니다! 친구한테 공유해보세요');
+    })
+    .catch(err => {
+      console.error('URL 복사에 실패했습니다.', err);
+    });
+}
+
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // ES6의 구조 분해 할당을 사용하여 요소를 교환
+  }
+}
+
+// url 이름 찾기
+function getFileName(url) {
+  // URL을 '/'로 분리하고 마지막 부분을 반환
+  return url.substring(url.lastIndexOf('/') + 1);
+}
+
+// 웹페이지 로드 시 배열을 섞고 게임을 시작합니다.
+window.onload = function() {
+
+  const currentUrl = window.location.href;
+  const currentFileName = getFileName(currentUrl);
+  if(currentFileName === '125cc.html'){
+    shuffleArray(motorcycles_125cc); // 후보 목록을 랜덤으로 섞습니다.
+    const selectedMotorcycles = motorcycles_125cc.slice(0, 16); // 상위 16개를 선택합니다.
+    startRound(selectedMotorcycles); // 선택된 16개로 이상형 월드컵 시작
+  }  else if(currentFileName === 'quarter.html'){
+    shuffleArray(motorcycles_quarter); // 후보 목록을 랜덤으로 섞습니다.
+    const selectedMotorcycles = motorcycles_quarter.slice(0, 16); // 상위 16개를 선택합니다.
+    startRound(selectedMotorcycles); // 선택된 16개로 이상형 월드컵 시작
+  } else if(currentFileName === 'middle.html'){
+    shuffleArray(motorcycles_middle); // 후보 목록을 랜덤으로 섞습니다.
+    const selectedMotorcycles = motorcycles_middle.slice(0, 16); // 상위 16개를 선택합니다.
+    startRound(selectedMotorcycles); // 선택된 16개로 이상형 월드컵 시작
+  } else if(currentFileName === 'dream.html'){
+    shuffleArray(motorcycles_dream); // 후보 목록을 랜덤으로 섞습니다.
+    const selectedMotorcycles = motorcycles_dream.slice(0, 16); // 상위 16개를 선택합니다.
+    startRound(selectedMotorcycles); // 선택된 16개로 이상형 월드컵 시작
+  }
+
+}
+
+
+
+
